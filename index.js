a1ffa09d3d81099b1997371bde37027513f59389
syko <sykolax@gmail.com>
2024-10-07 21:15:00 +0900

    feat: add function to generate random indicies

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x",
     "y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_",
     "-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+
+function generateRandomNumbers(num) {
+    let randomNums = [];
+    for(let i = 0; i < num; i++) {
+        randomNums.push(Math.floor(Math.random()*characters.length));
+    }
+    return randomNums;
+}
+
