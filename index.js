1a0c3569bbc87efa41fd8da8e953565bd854dec1
syko <sykolax@gmail.com>
2024-10-08 16:00:40 +0900

    feat: grab the elements from html file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_",
     "-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+let generateBtnEl = document.querySelector("#generate-btn");
+let passwordEls = document.querySelectorAll(".generated-password");
 
 function generateRandomNumbers(num) {
     let randomNums = [];
